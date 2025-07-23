987055fcf2fc80e7689ed0e58cb13653aa3b6491
Maximilian Spoljaric <spoljaricmax@gmail.com>
2025-07-23 14:23:09 +0300

    Make empty functions, variables req by assignment
    I made all the empty functions and variables.
    Wrote algorithm for getComputerChoice-function,
    tested with console logs. It works as intended,
    returns a choice of rock, paper or scissor.

--- javascipt.js
+++ javascipt.js
@@ -1,1 +1,38 @@
 console.log("Hello World!");
+
+let humanChoice;
+let computerChoice;
+let humanScore;
+let computerScore;
+
+function getComputerChoice() {
+    
+    let number = Math.floor(Math.random()*3);
+    console.log(number);
+        if(number==0){
+            computerChoice="rock";
+        }else if(number==1){
+            computerChoice="paper";
+        }else{
+            computerChoice="scissors"
+        }
+        console.log(computerChoice);
+    return computerChoice;
+}
+
+function getHumanChoice() {
+
+    return humanChoice;
+}
+
+function playRound(humanChoice,computerChoice) {
+
+
+}
+
+function playGame() {
+
+
+}
+
+console.log("The computer chose " + getComputerChoice());
