ee8321e5b2fc9392152b8bf661848939a6fe67f7
Maximilian Spoljaric <spoljaricmax@gmail.com>
2025-08-06 17:24:31 +0300

    Add div to present log of events
    Added a new div with id "log". Also added a new
    function, winnerChek to chek for a match winning
    score. implementing this required some reworking
    of the playGameUi function.

--- javascipt.js
+++ javascipt.js
@@ -29,39 +29,7 @@ function getHumanChoice() {
     
 }
 
-function playRound() {
-        const humanChoice = getHumanChoice();
-        const computerChoice =getComputerChoice();
 
-        console.log("playRound recieved human value: " + humanChoice);
-        console.log("playRound recieved computer value: " + computerChoice);
-
-        if( humanChoice=="rock"||
-            humanChoice=="paper"||
-            humanChoice=="scissors"||
-            humanChoice=="scissor"){
-
-            if((( humanChoice=="rock")&&(computerChoice=="scissors"||
-                  computerChoice=="scissor")) || 
-                  ((humanChoice=="paper")&&(computerChoice=="rock")) || 
-                  ((humanChoice=="scissor"||
-                  humanChoice=="scissors")&&(computerChoice=="paper"))){
-                return "human"
-              
-            }else if((( humanChoice=="scissors"||
-                        humanChoice=="scissor")&&computerChoice=="rock") || 
-                        (humanChoice=="rock"&&computerChoice=="paper") || 
-                        (humanChoice=="paper"&&(computerChoice=="scissor" || computerChoice=="scissors"))){
-                        return "computer";
-                                      
-            }else{
-                console.log("It's a tie! You both chose " + humanChoice + ".");
-            }
-        }else{
-            console.log("Incorret input. Reload page and try again.")
-
-        }
-    }
 
     function playRoundForUi(input) {
         const humanChoice = input;
@@ -89,7 +57,7 @@ function playRound() {
                         return "computer";
                                       
             }else{
-                console.log("It's a tie! You both chose " + humanChoice + ".");
+                log.innerHTML = "It's a tie! You both chose " + humanChoice + ".";
             }
         }else{
             console.log("Incorret input. Reload page and try again.")
@@ -97,45 +65,9 @@ function playRound() {
         }
     }
 
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let index = 0; index <= 4; index++) {
-        console.log("----ROUND " + (index+1)+"----");
-        
-        let winner = playRound();
-        
-        if (winner=="human") {
-            humanScore++
-            console.log("Score: Human " + humanScore + " - Computer " + computerScore);
-            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
-            
-            
-        }else if (winner=="computer") {
-            computerScore++
-            console.log("Score: Human " + humanScore + " - Computer " + computerScore);
-            console.log("You Lose! " + humanChoice + " does not beat " + computerChoice + ".");
-            
-        }
-            
-    }
-    if (humanScore>computerScore) {
-        console.log("You Win the Game!");
-        
-    }else if (computerScore>humanScore) {
-        console.log("Computer Wins the Game!");
-    }else{
-        console.log("It's A Tie! " + humanScore + " " + computerScore);
-    }
-
-}
 
 const results = document.getElementById("results");
-console.log(results);
-let testNumber=12;
-
+const log = document.getElementById("log");
 let scoreHuman = 0;
 let scoreComputer = 0;
 let round = 0;
@@ -163,19 +95,53 @@ function playGameForUi(input) {
 
 
 document.addEventListener("click", function(event){
+    
+  
+   // if(log.length>0)log.removeChild(log.firstChild);
     let choice = event.target.tagName.toLowerCase()
     if(choice == "button" ){
         let game = playGameForUi(event.target.id);
         if(game==1){
             scoreHuman += 1;
-            console.log("Score: Human " + scoreHuman + " - Computer " + scoreComputer);
-            console.log("You win! " + choice + " beats " + computerChoice + ".");
-            
+           // let logPara = document.createElement("p");
+            log.innerHTML = "You win! " + choice + " beats " + computerChoice + ".";
+            //log.appendChild(logPara);
+            switch (winnerChek(scoreHuman,scoreComputer)) {
+                case 1:
+                    log.innerHTML = "You win!"
+                    scoreComputer = 0; 
+                    scoreHuman = 0;
+                    break;
+                case 0:
+                    log.innerHTML = "Computer wins!"       
+                    scoreComputer = 0;
+                    scoreHuman = 0;
+                    break;  
+                case "not yet":
+                    
+                    break;
+            }  
+
         }else if(game==0){
             scoreComputer += 1;
-            console.log("Score: Human " + scoreHuman + " - Computer " + scoreComputer);
-            console.log("You Lose! " + choice + " does not beat " + computerChoice + ".");
-            
+           // let logPara = document.createElement("p");
+            log.innerHTML = "You Lose! " + choice + " does not beat " + computerChoice + ".";
+            //log.appendChild(logPara);
+            switch (winnerChek(scoreHuman,scoreComputer)) {
+                case 1:
+                    log.innerHTML = "You win!"
+                    scoreComputer = 0; 
+                    scoreHuman = 0;
+                    break;
+                case 0:
+                    log.innerHTML = "Computer wins!"       
+                    scoreComputer = 0;
+                    scoreHuman = 0;
+                    break;  
+                case "not yet":
+                    
+                    break;
+            }  
 
         }
          
@@ -184,6 +150,16 @@ document.addEventListener("click", function(event){
     results.innerHTML = textScore;
     
 });
+
+function winnerChek(human, computer){
+    if (human==5){
+        return 1;
+    }else if(computer==5){
+        return 0;
+    }else{
+        return "not yet";
+    }
+}
 
 
 // playGame();
